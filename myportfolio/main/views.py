7504a0c94b24408f5f6b4ede921bec0747def873
Eljen Mae Augusto <xt202001173@wmsu.edu.ph>
2024-08-03 22:08:59 +0800

    adding something not yet done
    Some changes a little bit

--- myportfolio/main/views.py
+++ myportfolio/main/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+def index(response):
+    return HttpResponse("<h1>tech with tim!</h1>")
+
+def v1(response):
+    return HttpResponse("<h1>tech with tim View 1!!</h1>")
