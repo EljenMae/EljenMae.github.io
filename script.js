88510b1c199c64ea8769ac242241d5f9fec6f432
Eljen Mae Augusto <86765650+EljenMae@users.noreply.github.com>
2024-11-03 11:58:10 +0800

    almost 80 percent

---
+++ script.js
@@ -0,0 +1,15 @@
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+let header = document.querySelector('header');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('active');
+}
+window.addEventListener('scroll', () => {
+    header.classList.toggle('shadow', window.scrollY > 0);
+});
